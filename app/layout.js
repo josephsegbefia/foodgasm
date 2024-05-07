e2320d0e0194ba0244a8455a8cf91428365947df
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-05-07 16:41:12 +0200

    Add fetch meals fxn

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import MainHeader from "@/components/main-header/main-header";
 import "./globals.css";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
 export const metadata = {
-  title: "Foodgasm",
+  title: "FoodGasm",
   description: "Delicious meals, shared by a food-loving community."
 };
 
